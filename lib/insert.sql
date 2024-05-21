be9580d16419792b816798e70d0b57c0bf341031
Mohamed Ahmed <ma07041705@gmail.com>
2024-05-21 09:06:57 +0300

    Organizing Bears Lab

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 40, 'M', 'Brown', 'sad', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 35, 'M', 'White', 'Harsh', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 20, 'M', 'Brown', 'nice', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 1, 'M', 'Brown', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 15, 'M', 'Brown', 'slimy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 50, 'M', 'Brown', 'calm', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 70, 'M', 'Brown', 'Happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 20, 'M', 'Brown', 'sad', true);
